92de162e9fd9603ee1b454b82071801dbb20ec7a
thegouge <thegougedev@gmail.com>
2024-06-14 21:16:48 -0700

    pulling the character sheets from the db onto the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
+import CharacterSheet from "~/components/CharacterSheet";
 import { db } from "~/server/db";
+import { Character } from "~/types"
 
 export const dynamic = "force-dynamic"
 
 export default async function HomePage() {
-	const posts = await db.query.posts.findMany()
+	const characters =  await db.query.characters.findMany()
 
 	return (
-		<main className="">
-			Hello! Welcome to DC20 Beyond! (WIP)
-			{posts.map(post => (
-				<div key={post.id}>{post.name}</div>
+		<main className="px-6">
+			<span className="block mb-4">Hello! Welcome to DC20 Beyond! (WIP)</span>
+			<div className="flex flex-wrap justify-between">
+			{characters.map((character)=>(
+			<div key={character.id} className="char-blurb p-6 bg-gray-50 text-black w-80 rounded">
+					<div>{character.char_name}</div>
+					<span className="char-slugline">Level: {character.char_level} | {character.char_ancestry} | {character.char_class}</span>
+				</div>
 			))}
+			</ div>
 		</main>
 	);
 }
