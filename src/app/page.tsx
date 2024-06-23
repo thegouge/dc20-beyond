03a9938518080bf2b7581a759f21eeec8352ecec
thegouge <thegougedev@gmail.com>
2024-06-22 23:55:54 -0700

    Linking the homepage to characters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,31 @@
-import CharacterSheet from "~/components/CharacterSheet";
+import Link from "next/link";
 import { db } from "~/server/db";
-import { Character } from "~/types"
 
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-	const characters =  await db.query.characters.findMany({
-		orderBy: (model, {desc}) => desc(model.createdAt)
-	})
+	const characters = await db.query.characters.findMany({
+		orderBy: (model, { desc }) => desc(model.createdAt),
+	});
 
 	return (
 		<main className="px-6">
-			<span className="block mb-4">Hello! Welcome to DC20 Beyond! (WIP)</span>
+			<span className="mb-4 block">Hello! Welcome to DC20 Beyond! (WIP)</span>
 			<div className="flex flex-wrap justify-between">
-			{characters.map((character)=>(
-			<div key={character.id} className="char-blurb p-6 bg-gray-50 text-black w-80 rounded">
-					<div>{character.char_name}</div>
-					<span className="char-slugline">Level: {character.char_level} | {character.char_ancestry} | {character.char_class}</span>
-				</div>
-			))}
-			</ div>
+				{characters.map((character) => (
+					<Link
+						key={character.id}
+						className="char-blurb w-80 rounded bg-gray-50 p-6 text-black"
+						href={`/character/${character.id}`}
+					>
+						<div>{character.char_name}</div>
+						<span className="char-slugline">
+							Level: {character.char_level} | {character.char_ancestry} |{" "}
+							{character.char_class}
+						</span>
+					</Link>
+				))}
+			</div>
 		</main>
 	);
 }
