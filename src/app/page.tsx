7d6a1b52705b8c3933925c031ea39a36fca21d86
thegouge <thegougedev@gmail.com>
2024-06-26 19:01:22 -0700

    Use Auth for Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,42 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function CharacterFetch() {
 	const characters = await db.query.characters.findMany({
 		orderBy: (model, { desc }) => desc(model.createdAt),
 	});
+	return (
+		<div className="flex flex-wrap justify-between">
+			{characters.map((character) => (
+				<Link
+					key={character.id}
+					className="char-blurb w-80 rounded bg-gray-50 p-6 text-black"
+					href={`/character/${character.id}`}
+				>
+					<div>{character.char_name}</div>
+					<span className="char-slugline">
+						Level: {character.char_level} | {character.char_ancestry} |{" "}
+						{character.char_class}
+					</span>
+				</Link>
+			))}
+		</div>
+	);
+}
 
+export default async function HomePage() {
 	return (
 		<main className="px-6">
-			<span className="mb-4 block">Hello! Welcome to DC20 Beyond! (WIP)</span>
-			<div className="flex flex-wrap justify-between">
-				{characters.map((character) => (
-					<Link
-						key={character.id}
-						className="char-blurb w-80 rounded bg-gray-50 p-6 text-black"
-						href={`/character/${character.id}`}
-					>
-						<div>{character.char_name}</div>
-						<span className="char-slugline">
-							Level: {character.char_level} | {character.char_ancestry} |{" "}
-							{character.char_class}
-						</span>
-					</Link>
-				))}
-			</div>
+			<h2 className="mb-4 block">Hello! Welcome to DC20 Beyond! (WIP)</h2>
+			<SignedOut>
+				<div className="h-full w-full text-2xl">Please sign in above</div>
+			</SignedOut>
+			<SignedIn>
+				<CharacterFetch />
+			</SignedIn>
 		</main>
 	);
 }
