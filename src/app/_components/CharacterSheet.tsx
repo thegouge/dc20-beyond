0c5d3502dcc40305944491cc66d55ed939ec2b79
thegouge <thegougedev@gmail.com>
2024-07-21 18:29:54 -0700

    Updating the Character Sheet with Interactivity

--- src/app/_components/CharacterSheet.tsx
+++ src/app/_components/CharacterSheet.tsx
@@ -20,18 +20,65 @@ type PropTypes = {
 };
 
 export default function heet({ character }: PropTypes) {
-  const [hp] = useState(useMaxHP(character));
-  const [stamina] = useState(useMaxStamina(character));
-  const [mana] = useState(useMaxMana(character));
-  const [actionPoints] = useState(4);
-  const [combatMastery] = useState(useCombatMastery(character));
-  const [exhaustionPoints] = useState(0);
-  const [restPoints] = useState(character.level + character.might);
-  const [gritPoints] = useState(2 + character.charisma);
+  const maxHP = useMaxHP(character);
+  const maxStamina = useMaxStamina(character);
+  const maxMana = useMaxMana(character);
+  const maxRest = character.level + character.might;
+  const maxGrit = 2 + character.charisma;
+  const combatMastery = useCombatMastery(character);
+  const deathThreshold = character.prime * -1;
 
-  console.log({character})
+  const [hp, setHP] = useState(maxHP);
+  const [tempHP, setTempHP] = useState(0);
+  const [stamina, setStamina] = useState(maxStamina);
+  const [mana, setMana] = useState(maxMana);
+  const [actionPoints, setActionPoints] = useState({
+    box1: true,
+    box2: true,
+    box3: true,
+    box4: true,
+  });
+  const [exhaustion, setExhaustion] = useState({
+    box1: false,
+    box2: false,
+    box3: false,
+    box4: false,
+    box5: false,
+  });
+  const [restPoints, setRest] = useState(maxRest);
+  const [gritPoints, setGrit] = useState(maxGrit);
+
+  const handleActionBoxChange = (
+    boxName: "box1" | "box2" | "box3" | "box4",
+  ) => {
+    const updatedCheckedBoxes = {
+      ...actionPoints,
+      [boxName]: !actionPoints[boxName],
+    };
+    setActionPoints(updatedCheckedBoxes);
+  };
+
+  const handleExhaustionBoxChange = (
+    boxName: "box1" | "box2" | "box3" | "box4" | "box5",
+  ) => {
+    const updatedCheckedBoxes = {
+      ...exhaustion,
+      [boxName]: !exhaustion[boxName],
+    };
+    setExhaustion(updatedCheckedBoxes);
+  };
+
+  const resetActionPoints = () => {
+    setActionPoints({
+      box1: true,
+      box2: true,
+      box3: true,
+      box4: true,
+    });
+  };
+
   return (
-    <article className="sheet grid grid-cols-3">
+    <article className="sheet grid grid-cols-3 p-4">
       <section className="top-block col-span-3 flex w-full justify-between">
         <div className="flex w-full flex-col">
           <div className="stat-box">
@@ -71,42 +118,24 @@ export default function heet({ character }: PropTypes) {
         </div>
       </section>
       {/* character.sidebar */}
-      <section className="stat-box row-span-2 flex flex-col">
+      <section className="stat-box row-span-3 flex flex-col">
         <div className="stat-box">
-          <SkillList
-            attribute="prime"
-            character={character}
-          />
+          <SkillList attribute="prime" character={character} />
         </div>
         <div className="stat-box">
-          <SkillList
-            attribute="might"
-            character={character}
-          />
+          <SkillList attribute="might" character={character} />
         </div>
         <div className="stat-box">
-          <SkillList
-            attribute="agility"
-            character={character}
-          />
+          <SkillList attribute="agility" character={character} />
         </div>
         <div className="stat-box">
-          <SkillList
-            attribute="charisma"
-            character={character}
-          />
+          <SkillList attribute="charisma" character={character} />
         </div>
         <div className="stat-box">
-          <SkillList
-            attribute="intelligence"
-            character={character}
-          />
+          <SkillList attribute="intelligence" character={character} />
           <div>
             <span>Knowledge</span>
-            <SkillList
-              attribute="knowledge"
-              character={character}
-            />
+            <SkillList attribute="knowledge" character={character} />
           </div>
         </div>
       </section>
@@ -114,10 +143,18 @@ export default function heet({ character }: PropTypes) {
         <div className="stat-box flex flex-col justify-between">
           <div className="text-center">
             Stamina Points
-            <br /> {stamina}
+            <br />
+            <input
+              className="number-flipper"
+              type="number"
+              value={stamina}
+              onChange={(e) => setStamina(parseInt(e.target.value))}
+              max={maxStamina}
+              min="0"
+            />
           </div>
           <div className="text-center">
-            {useMaxStamina(character)}
+            {maxStamina}
             <br /> Max
           </div>
         </div>
@@ -125,10 +162,17 @@ export default function heet({ character }: PropTypes) {
           <div className="text-center">
             Mana Points
             <br />
-            {mana}
+            <input
+              className="number-flipper"
+              type="number"
+              value={mana}
+              onChange={(e) => setMana(parseInt(e.target.value))}
+              max={maxMana}
+              min="0"
+            />
           </div>
           <div className="text-center">
-            {useMaxMana(character)}
+            {maxMana}
             <br />
             Max
           </div>
@@ -137,15 +181,29 @@ export default function heet({ character }: PropTypes) {
           <div className="text-center">
             Hit Points
             <br />
-            {hp}
+            <input
+              className="number-flipper"
+              type="number"
+              value={hp}
+              onChange={(e) => setHP(parseInt(e.target.value))}
+              max={maxHP}
+              min={deathThreshold}
+            />
           </div>
           <div className="text-center">
-            {useMaxHP(character)}
+            {maxHP}
             <br />
             Max
           </div>
           <div className="text-center">
-            {0}
+            <input
+              className="number-flipper"
+              type="number"
+              value={tempHP}
+              onChange={(e) => setTempHP(parseInt(e.target.value))}
+              max={maxHP}
+              min="0"
+            />
             <br />
             Temp
           </div>
@@ -169,53 +227,131 @@ export default function heet({ character }: PropTypes) {
         </div>
       </section>
       <section className="stat-box">
+        <div>
+          <div>
+            Action points:
+            <button
+              onClick={resetActionPoints}
+              className="ml-2 rounded border border-white p-2"
+            >
+              Reset Actions
+            </button>
+          </div>
+          <label className="my-6 flex justify-between">
+            <input
+              className="action-box"
+              type="checkbox"
+              checked={actionPoints.box1}
+              onChange={() => handleActionBoxChange("box1")}
+            />
+            <input
+              className="action-box"
+              type="checkbox"
+              checked={actionPoints.box2}
+              onChange={() => handleActionBoxChange("box2")}
+            />
+            <input
+              className="action-box"
+              type="checkbox"
+              checked={actionPoints.box3}
+              onChange={() => handleActionBoxChange("box3")}
+            />
+            <input
+              className="action-box"
+              type="checkbox"
+              checked={actionPoints.box4}
+              onChange={() => handleActionBoxChange("box4")}
+            />
+            <div></div>
+          </label>
+        </div>
+        <div>
+          <div> {combatMastery + character.prime} - Attack / Spell Check</div>
+          <div>{10 + combatMastery + character.prime} - Save DC</div>
+        </div>
+      </section>
+      <section className="stat-box row-span-2">
+        <div>
+          <div>exhaustion: </div>
+          <div className="my-6 flex justify-between">
+            <input
+              className="exhaustion-box"
+              type="checkbox"
+              checked={exhaustion.box1}
+              onChange={() => handleExhaustionBoxChange("box1")}
+            />
+            <input
+              className="exhaustion-box"
+              type="checkbox"
+              checked={exhaustion.box2}
+              onChange={() => handleExhaustionBoxChange("box2")}
+            />
+            <input
+              className="exhaustion-box"
+              type="checkbox"
+              checked={exhaustion.box3}
+              onChange={() => handleExhaustionBoxChange("box3")}
+            />
+            <input
+              className="exhaustion-box"
+              type="checkbox"
+              checked={exhaustion.box4}
+              onChange={() => handleExhaustionBoxChange("box4")}
+            />
+            <input
+              className="exhaustion-box"
+              type="checkbox"
+              checked={exhaustion.box5}
+              onChange={() => handleExhaustionBoxChange("box5")}
+            />
+            <div className="death-icon"></div>
+          </div>
+        </div>
+        <div>{deathThreshold} - Death Threshold</div>
+        <div>
+          <div>25ft - Move Speed</div>
+        </div>
+        <div>
+          <div>{5 * character.agility}ft - Jump distance</div>
+        </div>
+        <section className="stat-box">
+          <div>
+            <div>Rest Points:</div>
+            <input
+              className="number-flipper"
+              type="number"
+              value={restPoints}
+              onChange={(e) => setRest(parseInt(e.target.value))}
+              max={maxRest}
+              min="0"
+            />
+            / {maxRest}
+          </div>
+          <div>
+            <div>Grit Points:</div>
+            <input
+              className="number-flipper"
+              type="number"
+              value={restPoints}
+              onChange={(e) => setRest(parseInt(e.target.value))}
+              max={maxRest}
+              min="0"
+            />
+            / {maxGrit}
+          </div>
+        </section>
+      </section>
+      <section className="stat-box">
         <span>Trades</span>
         <div className="skill-line"></div>
         <div className="skill-line"></div>
         <div className="skill-line"></div>
         <div className="skill-line"></div>
-      </section>
-      <section className="stat-box">
         <span>Languages</span>
         <div className="skill-line"></div>
         <div className="skill-line"></div>
         <div className="skill-line"></div>
         <div className="skill-line"></div>
-      </section>
-      <section className="stat-box">
-        <div>
-          <div>Action points:</div>
-          <div>{actionPoints}</div>
-        </div>
-        <div>
-          <div> {combatMastery + character.prime}</div> Attack / Spell Check
-          <div>{10 + combatMastery + character.prime} Save DC</div>
-        </div>
-      </section>
-      <section className="stat-box">
-        <div>
-          <div>exhaustion: </div>
-          <div>{exhaustionPoints}</div>
-        </div>
-        <div>{character.prime * -1}: Death Threshold</div>
-        <div>
-          <div>25ft</div>
-          <div>Move Speed</div>
-        </div>
-        <div>
-          <div>{5 * character.agility}ft</div>
-          <div>Jump distance</div>
-        </div>
-      </section>
-      <section className="stat-box">
-        <div>
-          <div>Rest Points:</div>
-          {restPoints} / {character.level + character.might}
-        </div>
-        <div>
-          <div>Grit Points:</div>
-          {gritPoints} / {2 + character.charisma}
-        </div>
       </section>
     </article>
   );
