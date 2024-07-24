b24fdef60dd5f1340dc664feb22891bac7823a04
thegouge <thegougedev@gmail.com>
2024-07-23 18:44:31 -0700

    Actually adding links to the topnav

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,20 +1,21 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function TopNav() {
 	return (
 		<nav className="mb-8 flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
 			<div className="links flex w-1/4 items-center justify-between">
-				<div>Character Sheet</div>
-				<div>Form</div>
+				<Link href="/">Character Sheet</Link>
+				<Link href="/form">Form</Link>
 			</div>
-			<div>
+			{/*<div>
 				<SignedOut>
 					<SignInButton />
 				</SignedOut>
 				<SignedIn>
 					<UserButton />
 				</SignedIn>
-			</div>
+			</div>*/}
 		</nav>
 	);
 }
