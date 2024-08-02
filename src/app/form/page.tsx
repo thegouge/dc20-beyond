154b6388a0b7a1734e64a89373f4118554593ddf
Alex Gouge-Schajer <thegougedev@gmail.com>
2024-08-01 20:47:46 -0700

    Trying to pass form updates up to the page

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,16 +1,24 @@
-import Attributes from "~/app/_components/Attributes";
+import Attributes from "~/app/_components/form/Attributes";
 
 export default function Form() {
-	const fromSteps = [
-		"Attributes", // attributes & saves
-		"Background", // skills, trades, languages
-		"Ancestry",
-		"Class",
-		"Weapons",
-	];
-	return (
-		<main>
-			<Attributes />
-		</main>
-	);
+  const fromSteps = [
+    "Attributes", // attributes & saves
+    "Background", // skills, trades, languages
+    "Ancestry",
+    "Class",
+    "Weapons",
+  ];
+
+  const aggregate = {};
+
+  function advance(stage, payload) {
+    aggregate[stage] = payload;
+    console.log({ aggregate });
+  }
+
+  return (
+    <main>
+      <Attributes advance={(e) => advance("attributes", e)} />
+    </main>
+  );
 }
