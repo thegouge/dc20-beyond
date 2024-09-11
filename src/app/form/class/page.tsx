9618788a33768d7a7feeaa23843683314ceab0a2
Alex Gouge-Schajer <thegougedev@gmail.com>
2024-09-11 12:23:49 -0700

    Building a basic version of Class Selection

--- src/app/form/class/page.tsx
+++ src/app/form/class/page.tsx
@@ -1,37 +1,45 @@
 "use client";
 
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { CLASSES } from "~/constants";
 import { CharacterContext } from "~/helpers/characterContext";
+import { PlayerClasses } from "~/types";
+import "~/styles/traits.css";
+
+const ALL_CLASSES = Object.keys(CLASSES)
 
 export default function ClassPage() {
   const character = useContext(CharacterContext);
+  const [chosenClass, setChosenClass] = useState("")
+
+  function toggleSelected(chosen: string) {
+    if (chosenClass === chosen) {
+      setChosenClass("")
+    } else {
+      setChosenClass(chosen)
+    }
+  }
+
+  function handleSubmit(e: any) {
+    e.preventDefault()
+
+    console.log({ chosenClass })
+  }
 
   return (
     <div>
-      Class!
-      <br />
-      Name: {character.name}
-      <br />
-      Player: {character.playerName}
-      <br />
-      Prime: {character.prime}
-      <br />
-      Might: {character.might}
-      <br />
-      Agility: {character.agility}
-      <br />
-      Charisma: {character.charisma}
-      <br />
-      Intelligence: {character.intelligence}
-      <br />
-      Saves: {character.attributeSaveMasteries}
-      <br />
-      Skills: {JSON.stringify(character.skillMasteries)}
-      <br />
-      Trades: {JSON.stringify(character.trades)}
-      <br />
-      Languages: {JSON.stringify(character.languages)}
-      <br />
+      <h2>Class!</h2>
+      <form onSubmit={handleSubmit}>
+        {ALL_CLASSES.map((className: string) => {
+          const currClass = CLASSES[className as PlayerClasses]
+          return (<div key={className} className={chosenClass === className ? "trait-selector selected" : "trait-selector unselected"} onClick={() => toggleSelected(className)}>
+            <h3>{currClass.name}</h3>
+            <p>{currClass.description}</p>
+          </div>)
+        })
+        }
+        <button type="submit" className="outline-button">Submit</button>
+      </form>
     </div>
   );
 }
